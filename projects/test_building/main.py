e975ee98bbd2e29395fc714c622be11de01314ea
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-03-16 01:33:51 +0100

    project name changed

--- projects/test_building/main.py
+++ projects/test_building/main.py
@@ -3,8 +3,8 @@ import os
 from Melodie import Config
 
 from models.render_building.main import run_building_model
-from projects.rokig.toolkit.table_processing import concat_region_tables
-from projects.rokig.toolkit.table_processing import find_id
+from projects.test_building.toolkit.table_processing import concat_region_tables
+from projects.test_building.toolkit.table_processing import find_id
 
 
 def get_config(project_name: str):
@@ -19,7 +19,7 @@ def get_config(project_name: str):
 
 
 if __name__ == "__main__":
-    config = get_config("rokig")
+    config = get_config("test_building")
     # run_building_model(cfg=config)
     # concat_region_tables(cfg=config, file_name_prefix="energy_consumption")
     find_id(cfg=config, id_name="id_subsector")
