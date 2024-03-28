4060063879cf08cd7cf7ad69a711147317591831
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-03-28 23:04:21 +0100

    mac and lvm memory log plotted

--- docs/parse_memory_log.py
+++ docs/parse_memory_log.py
@@ -1,6 +1,6 @@
 from typing import List
 import pandas as pd
-
+import os
 
 def parse_memory_log(file_names: List[str]):
     for file_name in file_names:
@@ -10,11 +10,21 @@ def parse_memory_log(file_names: List[str]):
             for line in lines:
                 memory = line.split()[-2]
                 memory_usage.append(int(memory)/1024**2)
-            pd.DataFrame({file_name: memory_usage}).to_excel(f"{file_name}.xlsx", index=False)
+            df = pd.DataFrame({file_name: memory_usage})
+            df["unit"] = "GB"
+            sheet_name = "_".join(file_name.split("_")[0:3])
+            target_file = "memory_track.xlsx"
+            if os.path.exists(target_file):
+                with pd.ExcelWriter(target_file, engine='openpyxl', mode='a') as writer:
+                    df.to_excel(writer, sheet_name=sheet_name, index=False)
+            else:
+                df.to_excel(target_file, sheet_name=sheet_name, index=False)
 
 
 if __name__ == "__main__":
     parse_memory_log(file_names=[
-        "lvm_memory_no_cache_103min_384scenarios",
-        "lvm_memory_with_cache_62min_267scenarios",
+        "lvm_no_cache_103min_384scenarios",
+        "lvm_with_cache_62min_267scenarios",
+        "mac_with_cache_44min_401scenarios",
+        "mac_no_cache_44min_401scenarios",
     ])
