3e9d4b9ec9eb65407ffc484eee690182c6226812
Songmin <yu.ceepcas@gmail.com>
2024-03-28 22:41:00 +0100

    memory monitor in the unit of MB

--- docs/monitor_python_memory_win.bat
+++ docs/monitor_python_memory_win.bat
@@ -1,17 +1,16 @@
 @echo off
 
 :loop
-set "total_memory_kb=0"
+set "total_memory_mb=0"
 for /f "skip=1 tokens=*" %%a in (
 'wmic process where "Name like '%%Python%%'" get WorkingSetSize ^| findstr /r "[0-9]"'
 ) do (
     set "memory_bytes=%%a"
-    set /a "memory_kb=memory_bytes / 1024"
-    set /a "total_memory_kb+=memory_kb"
+    set /a "memory_mb=memory_bytes / 1024 / 1024"
+    set /a "total_memory_mb+=memory_mb"
 )
 
-rem Output total memory usage in GB
-echo Total memory usage of Python processes: %total_memory_kb% KB
+echo Total memory usage of Python processes: %total_memory_mb% MB
 
 timeout /t 1 >nul
 goto loop
