4440e05c7f6b755ab804a1fa1babe0a98077c7b4
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-04-01 21:11:50 +0200

    table processor moved to utils

--- projects/test_building/main.py
+++ projects/test_building/main.py
@@ -3,8 +3,7 @@ import os
 from Melodie import Config
 
 from models.render_building.main import run_building_model
-from projects.test_building.toolkit.table_processing import concat_region_tables
-from projects.test_building.toolkit.table_processing import find_id
+from utils import table_processor
 
 
 def get_config(project_name: str):
@@ -19,8 +18,10 @@ def get_config(project_name: str):
 
 
 def run_tool(cfg: "Config"):
-    # concat_region_tables(cfg=cfg, file_name_prefix="AgentNumAgg")
-    find_id(cfg=cfg, id_name="id_building_action")
+    # table_processor.concat_region_tables(cfg=cfg, file_name_prefix="AgentNumAgg")
+    # table_processor.find_id(cfg=cfg, id_name="id_building_action")
+    # table_processor.extract_id_data(cfg=cfg, id_name="id_region", id_value=9010101)
+    table_processor.pack_sqlite(cfg=cfg)
 
 
 if __name__ == "__main__":
--- utils/table_processor.py
+++ utils/table_processor.py
@@ -1,5 +1,5 @@
 import os
-
+import sqlite3
 import pandas as pd
 from Melodie import Config
 
@@ -10,6 +10,13 @@ def read_dataframe(file_path: str):
     else:
         df = pd.read_csv(file_path)
     return df
+
+
+def get_data_files(folder: str):
+    return [
+        file for file in os.listdir(folder) if
+        file.endswith(".xlsx") or file.endswith(".csv")
+    ]
 
 
 def concat_region_tables(cfg: "Config", file_name_prefix: str):
@@ -32,23 +39,44 @@ def concat_region_tables(cfg: "Config", file_name_prefix: str):
             df_sector.to_csv(os.path.join(cfg.output_folder, f"{file_name_prefix}_Sector{id_sector}.csv"), index=False)
 
 
-def find_id(cfg: "Config", id_name: str):
+def get_id_relevant_tables(cfg: "Config", id_name: str):
 
-    def get_data_files():
-        return [
-            file for file in os.listdir(cfg.input_folder) if
-            file.endswith(".xlsx") or file.endswith(".csv")
-        ]
-
-    files = []
-    print(f'Following tables include `{id_name}` --> ')
-    for file_name in get_data_files():
-        df = pd.read_excel(os.path.join(cfg.input_folder, file_name), engine='openpyxl')
+    tables = {}
+    for file_name in get_data_files(cfg.input_folder):
+        df = read_dataframe(os.path.join(cfg.input_folder, file_name))
         if id_name in list(df.columns):
-            files.append(file_name)
-    if files:
-        for file_name in files:
+            tables[file_name] = df
+    return tables
+
+
+def find_id(cfg: "Config", id_name: str):
+    tables = get_id_relevant_tables(cfg=cfg, id_name=id_name)
+    if tables:
+        print(f'Following tables include `{id_name}` --> ')
+        for file_name in tables.keys():
             print(file_name)
     else:
         print(f"No table is relevant to {id_name}.")
-    return files
+
+
+def extract_id_data(cfg: "Config", id_name: str, id_value: int):
+    tables = get_id_relevant_tables(cfg=cfg, id_name=id_name)
+    for table_name, table_df in tables.items():
+        table_df = table_df.loc[table_df[id_name] == id_value]
+        file_name, ext = table_name.split(".")
+        table_df.to_csv(os.path.join(cfg.output_folder, f'{file_name}_{id_name}_{id_value}.csv'), index=False)
+
+
+def pack_sqlite(cfg: "Config"):
+    file_names = get_data_files(cfg.input_folder)
+    conn = sqlite3.connect(os.path.join(cfg.output_folder, f'{cfg.project_name}.sqlite'))
+    for file_name in file_names:
+        df = read_dataframe(os.path.join(cfg.input_folder, file_name))
+        try:
+            df.to_sql(name=file_name.split('.')[0], con=conn, if_exists='replace', index=False)
+        except sqlite3.OperationalError as e:
+            print(f"Error: {e}")
+    conn.close()
+
+
+
